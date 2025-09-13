45dab6e4fb7a8ffb47961b63863a1ded19028c74
pkluber <gkluber@sbcglobal.net>
2025-09-13 12:32:25 +0800

    refactor Density

--- density/density.py
+++ density/density.py
@@ -25,66 +25,51 @@ class Density():
         self.rho = rho
         self.unitcell = unitcell
         self.grid = grid
-
-        #TODO incorporate origin
         self.origin = origin
 
-    def mesh_3d(self, rmin=[0, 0, 0], rmax=0, scaled = False, pbc = True, indexing = 'xy'):
+    def mesh_3d(self, rmax: np.ndarray | None = None, scaled: bool = False):
         """
         Returns a 3d mesh taking into account periodic boundary conditions
 
         Parameters
         ----------
 
-        rmin, rmax: list
-            lower and upper cutoff in every euclidean direction.
+        rmax: list
+            upper cutoff in every euclidean direction.
         scaled: boolean
             scale the meshes with unitcell size?
-        pbc: boolean
-            assume periodic boundary conditions?
-        indexing: 'xy' or 'ij'
-            indexing scheme used by np.meshgrid.
 
         Returns
         -------
 
         X, Y, Z: tuple of np.ndarray
-            defines mesh.
-        """
-
-        if rmax == 0:
-            mid_grid = np.floor(self.grid / 2).astype(int)
-            rmax = mid_grid
-
-        # resolve the periodic boundary conditions
-        if pbc:
-            x_pbc = list(range(-rmax[0], -rmin[0])) + list(range(rmin[0], rmax[0]+1))
-            y_pbc = list(range(-rmax[1], -rmin[1])) + list(range(rmin[1], rmax[1]+1))
-            z_pbc = list(range(-rmax[2], -rmin[2])) + list(range(rmin[2], rmax[2]+1))
-        else:
-            x_pbc = list(range(rmin[0], rmax[0] +1 )) + list(range(-rmax[0], -rmin[0]))
-            y_pbc = list(range(rmin[1], rmax[1] +1 )) + list(range(-rmax[1], -rmin[1]))
-            z_pbc = list(range(rmin[2], rmax[2] +1 )) + list(range(-rmax[2], -rmin[2]))
-
-
-        Xm, Ym, Zm = np.meshgrid(x_pbc, y_pbc, z_pbc, indexing = indexing)
-
-        U = np.array(self.unitcell) # Matrix to go from real space to mesh coordinates
+            defines mesh in real space
+        """ 
+        U = np.array(self.unitcell)  # Matrix to go from real space to mesh coordinates
         for i in range(3):
             U[i,:] = U[i,:] / self.grid[i]
 
-        a = np.linalg.norm(self.unitcell, axis = 1)/self.grid[:3]
+        if rmax is not None:
+            # Convert rmax to grid coordinates
+            rmax = np.rint(np.dot(U, rmax)).astype(int)
+        else: 
+            # If no rmax, take the entire grid 
+            rmax = np.floor(self.grid / 2).astype(int)  
+
+        x, y, z = [list(range(-rmax[i], rmax[i]+1)) for i in range(3)]
+        
+        Xm, Ym, Zm = np.meshgrid(x, y, z, indexing='ij')
+        
+        if not scaled:
+            return Xm, Ym, Zm
 
         Rm = np.concatenate([Xm.reshape(*Xm.shape,1),
                              Ym.reshape(*Xm.shape,1),
                              Zm.reshape(*Xm.shape,1)], axis = 3)
-
-        if scaled:
-            R = np.einsum('ij,klmj -> iklm', U.T , Rm)
-            X = R[0,:,:,:]
-            Y = R[1,:,:,:]
-            Z = R[2,:,:,:]
-            return X,Y,Z
-        else:
-            return Xm,Ym,Zm
-
+        
+        R = np.einsum('ij,klmj -> iklm', U.T , Rm)
+        X = R[0,:,:,:] + self.origin
+        Y = R[1,:,:,:] + self.origin
+        Z = R[2,:,:,:] + self.origin
+        
+        return X, Y, Z 
