f7ba583e0eccf703abcbf47c8075a95b19b8ca31
pkluber <gkluber@sbcglobal.net>
2025-10-08 21:20:04 +0800

    refactor model.py UEDDIENetwork to use internal moduledicts instead of subnetworks

--- model.py
+++ model.py
@@ -171,7 +171,7 @@ class SwiGLU(nn.Module):
 
 # Multi-head self-attention implementation
 class MultiHeadSelfAttention(nn.Module):
-    def __init__(self, d_model: int, num_heads: int):
+    def __init__(self, d_model: int, num_heads: int, labels: list[int]):
         super().__init__()
         self.d_model = d_model
         self.num_heads = num_heads
@@ -181,13 +181,20 @@ class MultiHeadSelfAttention(nn.Module):
         self.d_head = self.d_padded // num_heads
         
         # QKV projections with padded dimension
-        self.qkv = nn.Linear(d_model, 3*self.d_padded)
+        self.qkvs = nn.ModuleDict({str(l): nn.Linear(d_model, 3*self.d_padded) for l in labels})
         self.proj = nn.Linear(self.d_padded, d_model)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        B, N, _ = x.shape
+    def forward(self, X: torch.Tensor, L: torch.Tensor) -> torch.Tensor:
+        B, N, _ = X.shape
+        
+        # Apply a different QKV mapping for each label
+        qkv = torch.zeros((B, N, 3*self.d_padded), device=X.device)
+        for l in self.qkvs.keys():
+            mask, X_masked = create_mask(X, L == int(l))
+            qkv_l = self.qkvs[l](X_masked)
+            qkv += qkv_l
 
-        qkv = self.qkv(x)
+        # Now chunk into q, k, v 
         q, k, v = qkv.chunk(3, dim=-1)
 
         # Reshape to [B, num_heads, N, d_head]
@@ -207,72 +214,70 @@ class MultiHeadSelfAttention(nn.Module):
         return self.proj(out)
 
 class UEDDIETransformerBlock(nn.Module):
-    def __init__(self, d_model: int, num_heads: int, d_ff: int):
+    def __init__(self, d_model: int, num_heads: int, d_ff: int, labels: list[int]):
         super().__init__()
         self.norm1 = nn.LayerNorm(d_model)
-        self.attn = MultiHeadSelfAttention(d_model, num_heads)
+        self.attn = MultiHeadSelfAttention(d_model, num_heads, labels)
         self.norm2 = nn.LayerNorm(d_model)
-        self.ff = SwiGLU(d_model, d_ff)
+        self.ffs = nn.ModuleDict({str(l): SwiGLU(d_model, d_ff) for l in labels})
 
-    def forward(self, x):
+    def forward(self, X: torch.Tensor, L: torch.Tensor):
         # Pre-norm then apply MHSA
-        x = self.norm1(x)
-        attn_out = self.attn(x)
+        X = self.norm1(X)
+        attn_out = self.attn(X, L)
         
         # Residual connection to another pre-norm then apply SwiGLU 
-        x = self.norm2(x + attn_out)
-        ff_out = self.ff(x)
+        X = self.norm2(X + attn_out)
+
+        ff_out = torch.zeros(X.shape, device=X.device)
+        for l in self.ffs.keys():
+            mask, X_masked = create_mask(X, L == int(l))
+            ff_l = self.ffs[l](X_masked)
+            ff_out += ff_l
         
         # Return residual of SwiGLU and pre-normed MHSA
-        return x + ff_out
+        return X + ff_out
 
-# Subnet of transformer blocks that projects to 1 dim at the end 
-class UEDDIESubnet(nn.Module):
-    def __init__(self, d_model: int, num_heads: int, d_ff: int, depth: int):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Sequential(*[UEDDIETransformerBlock(d_model, num_heads, d_ff) for _ in range(depth)]),
-            nn.Linear(d_model, 1)
-        )
-
-    def forward(self, X: torch.Tensor) -> torch.Tensor:
-        return self.net(X).squeeze(-1)
-
-
+VALID_ELEMENTS = list(range(4))
+VALID_CHARGES = list(range(-1, 2))
 class UEDDIENetwork(nn.Module):
     def __init__(self, d_model: int, num_heads: int = 4, d_ff: int = 128, depth_e: int = 10, depth_c: int = 10):
         super().__init__()
         
-        self.elem_subnets = nn.ModuleDict({str(e): UEDDIESubnet(d_model, num_heads, d_ff, depth_e) for e in range(4)})
-        self.charge_subnets = nn.ModuleDict({str(c): UEDDIESubnet(d_model, num_heads, d_ff, depth_c) for c in range(-1, 2)})
-           
+        self.elem_blocks = nn.ModuleList([UEDDIETransformerBlock(d_model, num_heads, d_ff, VALID_ELEMENTS) for _ in range(depth_e)])
+        self.elem_projs = nn.ModuleDict({str(e): nn.Linear(d_model, 1) for e in VALID_ELEMENTS})
+
+        self.charge_blocks = nn.ModuleList([UEDDIETransformerBlock(d_model, num_heads, d_ff, VALID_CHARGES) for _ in range(depth_c)])
+        self.charge_projs = nn.ModuleDict({str(c): nn.Linear(d_model, 1) for c in VALID_CHARGES})
+ 
     def forward(self, X: torch.Tensor, E: torch.Tensor, C: torch.Tensor) -> torch.Tensor:
         # Sanitize E, C, make sure they're int
         E = E.to(torch.int64)
         C = C.to(torch.int64)
 
-        # Atomistic approximation
-        per_atom_IE = torch.zeros(X.shape[:-1], device=X.device)
-
-        # Apply element subnets
-        energy_parts = []
-
-        for e in self.elem_subnets.keys():
-            mask, X_masked = create_mask(X, E == int(e))
-            energy_parts.append((mask, self.elem_subnets[e](X_masked)))
-
-        for mask_e, energy_e in energy_parts:
-            per_atom_IE = torch.where(mask_e[:, :, 0], energy_e, per_atom_IE)
-
-        # Apply charge scaling subnets
-        charge_parts = []
-
-        for c in self.charge_subnets.keys():
-            mask, X_masked = create_mask(X, C == int(c))
-            charge_parts.append((mask, per_atom_IE * torch.exp(self.charge_subnets[c](X_masked))))
+        # Element stream
+        e_out = X
+        for block in self.elem_blocks:
+            e_out = block(e_out, E)
         
-        for mask_c, energy_c in charge_parts:
-            per_atom_IE = torch.where(mask_c[:, :, 0], energy_c, per_atom_IE)
+        e_proj_out = torch.zeros(X.shape[:-1], device=X.device)
+        for e in self.elem_projs.keys():
+            mask, X_masked = create_mask(X, E == int(e))
+            proj_e = self.elem_projs[e](X_masked).squeeze(-1)
+            e_proj_out = torch.where(mask[:, :, 0], proj_e, e_proj_out)
+
+        # Charge scaling stream
+        c_out = X
+        for block in self.charge_blocks:
+            c_out = block(c_out, C)
+        
+        c_proj_out = torch.zeros(X.shape[:-1], device=X.device)
+        for c in self.charge_projs.keys():
+            mask, X_masked = create_mask(X, C == int(c))
+            proj_c = self.charge_projs[c](X_masked).squeeze(-1)
+            c_proj_out = torch.where(mask[:, :, 0], proj_c, c_proj_out)
+
+        per_atom_IE = e_proj_out * torch.exp(c_proj_out)
 
         self.per_atom_IE = -per_atom_IE
 
